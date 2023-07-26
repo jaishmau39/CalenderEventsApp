9828457a612a0201c159a910ce0764efdabaa670
Jaishma Usha <jaishmarajan8@gmail.com>
2023-07-26 14:37:48 -0400

    Add Vue.js Calendar app

---
+++ calendar-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <EventCalendar msg="Welcome to Your Events App"/>
+</template>
+
+<script>
+import EventCalendar from './components/EventCalendar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    EventCalendar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
